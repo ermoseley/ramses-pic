ermoseley/ramses-pic

commit d93027ff5c74ab1973ce5d3ae6f0eb74738d9d36

[tests] Add a -r option to test restart (#110)
Adds an option to interject a restart in all tests. The option is NOT
activated by default on the CI.


Some tests are failing because RAMSES is not binary exact. A solution
would be to store conservative variable instead of primitive ones (as
done in RAMSES ism) (see #123).
There is a more serious issue with the MC tracer test (see #121)

---------

Co-authored-by: Noe Brucy <noe.brucy@ens-lyon.fr>

tests/run_test_suite.sh
@@ -23,6 +23,10 @@
 #       ./run_test_suite.sh -t mhd
 #   - Run quick test suite:
 #       ./run_test_suite.sh -q
+#   - Run test suite with coverage:
+#       ./run_test_suite.sh -s
+#   - Run tests with restart:
+#       ./run_test_suite.sh -r
 #
 #######################################################################
 
@@ -40,7 +44,8 @@ DELDATA=true;
 COVERAGE=false;
 CLEAN_ALL=false;
 SELECTTEST=false;
-while getopts "cdsp:qt:v" OPTION; do
+RESTART=false;
+while getopts "cdsp:qt:vr" OPTION; do
    case $OPTION in
       c)
          CLEAN_ALL=true;
@@ -62,6 +67,9 @@ while getopts "cdsp:qt:v" OPTION; do
       ;;
       v)
          VERBOSE=true;
+      ;;
+      r)
+         RESTART=true;
       ;;
    esac
 done
@@ -305,24 +313,37 @@ for ((i=0;i<$ntests;i++)); do
    # Run test
    cd ${TEST_DIRECTORY}/${testname[n]};
    $DELETE_RESULTS;
-   RUN_TEST="${RUN_TEST_BASE}${ndim}d ${rawname[i]}.nml";
-   echo -n "Running test:" | tee -a $LOGFILE;
-   STARTTIME_TEST=$(python3 -c 'import time; print(int(time.time()*1000))');
-   # prepname="prepare-${rawname[i]}.sh";
+
    if $VERBOSE ; then
-      if [ -f ${BEFORETEST} ]; then
-         ${SHELL} ${BEFORETEST} 2>&1 | tee -a $LOGFILE;
-      fi
-      ${RUN_TEST} 2>&1 | tee -a $LOGFILE;
+      RUN_BEFORE_TEST="${SHELL} ${BEFORETEST} 2>&1 | tee -a ${LOGFILE}"
+      RUN_TEST="${RUN_TEST_BASE}${ndim}d ${rawname[i]}.nml 2>&1 | tee -a ${LOGFILE}"
    else
-      if [ -f ${BEFORETEST} ]; then
-         ${SHELL} ${BEFORETEST} >> $LOGFILE 2>&1;
-      fi
-      ${RUN_TEST} >> $LOGFILE 2>&1;
+      RUN_BEFORE_TEST="${SHELL} ${BEFORETEST} >> ${LOGFILE} 2>&1"
+      RUN_TEST="${RUN_TEST_BASE}${ndim}d ${rawname[i]}.nml  >> $LOGFILE 2>&1"
    fi
+   echo "Running test:" | tee -a $LOGFILE;
+   STARTTIME_TEST=$(python3 -c 'import time; print(int(time.time()*1000))');
+
+   if [ -f ${BEFORETEST} ]; then
+         ${RUN_BEFORE_TEST};
+   fi
+
+   if $RESTART ; then
+      echo  "Restart: step 1 ..." | tee -a $LOGFILE;
+      python3 ../../run_with_restart.py -s 1 -t ${rawname[i]}  | tee -a $LOGFILE;
+      ${RUN_TEST};
+      echo  "Restart: step 2 ..." | tee -a $LOGFILE;
+      python3 ../../run_with_restart.py -s 2 -t ${rawname[i]}  | tee -a $LOGFILE;
+      ${RUN_TEST};
+      echo  "Restart: step 3 ..." | tee -a $LOGFILE;
+      python3 ../../run_with_restart.py -s 3 -t ${rawname[i]}  | tee -a $LOGFILE;
+   else
+      ${RUN_TEST};
+   fi
+
    # Record test time
    ENDTIME_TEST=$(python3 -c 'import time; print(int(time.time()*1000))');
-   milliseconds=$(($ENDTIME_TEST - $STARTTIME_TEST));
+   milliseconds=$((${ENDTIME_TEST} - ${STARTTIME_TEST}));
    seconds=$(($milliseconds / 1000));
    hours=$(($seconds / 3600));
    seconds=$(($seconds % 3600));

tests/hydro/barotrop/barotrop.nml
@@ -45,7 +45,8 @@ p_region=2.418894075235192,24188.94075235192
 /
 
 &OUTPUT_PARAMS
-tend=0.00049
+noutput=1
+tout=0.00049
 /
 
 &UNITS_PARAMS

tests/hydro/cooling-frig/cooling-frig.nml
@@ -36,8 +36,8 @@ p_region=319266 ! 8000 K
 /
 
 &OUTPUT_PARAMS
-noutput=2
-tout=0.0,0.62  ! 100Myr
+noutput=1
+tout=0.62  ! 100Myr
 /
 
 &UNITS_PARAMS

tests/hydro/implosion/implosion.nml
@@ -37,8 +37,8 @@ p_region=1.0,0.4
 /
 
 &OUTPUT_PARAMS
-noutput=2
-tout=0.0,5.0
+noutput=1
+tout=5.0
 /
 
 &HYDRO_PARAMS

tests/hydro/isothermal/isothermal.nml
@@ -44,7 +44,8 @@ p_region=2.418894075235192,24188.94075235192
 /
 
 &OUTPUT_PARAMS
-tend=0.00049
+noutput=1
+tout=0.00049
 /
 
 &UNITS_PARAMS

tests/hydro/mixing-scalar/mixing-scalar.nml
@@ -1,7 +1,7 @@
 This is the RAMSES parameter file for the 2D passive scalar mixing test.
 A diamond shape is placed in a lower density medium which is moving to the left.
 The passive scalar is set to be 1 in the diamond shape and -1 in the background medium.
-Turbulance is generated in the wake at the corners of the diamond,
+Turbulence is generated in the wake at the corners of the diamond,
 causing the passive scalar to mix.
 
 &RUN_PARAMS

tests/mhd/imhd-tube-nener/imhd-tube-nener.nml
@@ -43,7 +43,6 @@ C_region=0.0,0.141120
 /
 
 &OUTPUT_PARAMS
-foutput=1000000
 noutput=1
 tout=0.4
 /

tests/mhd/imhd-tube/imhd-tube.nml
@@ -39,7 +39,6 @@ C_region=0.0,0.141120
 /
 
 &OUTPUT_PARAMS
-foutput=1000000
 noutput=1
 tout=0.4
 /

commit fa2b7be60b2e1f91f3b8fbda79162cf606c7af6a

[tests] solve bug where execution test log was not written (#125)
Solve a bug introduced by #110 that prevents the log of the execution of
the tests to be written on log.

Co-authored-by: Noé Brucy <noe.brucy@ens-lyon.fr>

tests/run_test_suite.sh
@@ -315,30 +315,42 @@ for ((i=0;i<$ntests;i++)); do
    $DELETE_RESULTS;
 
    if $VERBOSE ; then
-      RUN_BEFORE_TEST="${SHELL} ${BEFORETEST} 2>&1 | tee -a ${LOGFILE}"
-      RUN_TEST="${RUN_TEST_BASE}${ndim}d ${rawname[i]}.nml 2>&1 | tee -a ${LOGFILE}"
+      function run_before_test
+      {
+         (${SHELL} ${BEFORETEST} 2>&1 | tee -a ${LOGFILE})
+      }
+      function run_test
+      {
+         (${RUN_TEST_BASE}${ndim}d ${rawname[i]}.nml 2>&1 | tee -a ${LOGFILE})
+      }
    else
-      RUN_BEFORE_TEST="${SHELL} ${BEFORETEST} >> ${LOGFILE} 2>&1"
-      RUN_TEST="${RUN_TEST_BASE}${ndim}d ${rawname[i]}.nml  >> $LOGFILE 2>&1"
+      function run_before_test
+      {
+         (${SHELL} ${BEFORETEST} >> ${LOGFILE} 2>&1)
+      }
+      function run_test
+      {
+         (${RUN_TEST_BASE}${ndim}d ${rawname[i]}.nml >> ${LOGFILE} 2>&1)
+      }
    fi
    echo "Running test:" | tee -a $LOGFILE;
    STARTTIME_TEST=$(python3 -c 'import time; print(int(time.time()*1000))');
 
    if [ -f ${BEFORETEST} ]; then
-         ${RUN_BEFORE_TEST};
+         run_before_test;
    fi
 
    if $RESTART ; then
       echo  "Restart: step 1 ..." | tee -a $LOGFILE;
       python3 ../../run_with_restart.py -s 1 -t ${rawname[i]}  | tee -a $LOGFILE;
-      ${RUN_TEST};
+      run_test;
       echo  "Restart: step 2 ..." | tee -a $LOGFILE;
       python3 ../../run_with_restart.py -s 2 -t ${rawname[i]}  | tee -a $LOGFILE;
-      ${RUN_TEST};
+      run_test;
       echo  "Restart: step 3 ..." | tee -a $LOGFILE;
       python3 ../../run_with_restart.py -s 3 -t ${rawname[i]}  | tee -a $LOGFILE;
    else
-      ${RUN_TEST};
+      run_test;
    fi
 
    # Record test time